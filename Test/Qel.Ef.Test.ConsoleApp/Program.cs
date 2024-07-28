3c58701dd3d3486c14d1988ea06fc4fe600d53e4
SolW <kllykov@outlook.com>
2024-07-28 07:01:26 +0300

    Cosmetic commit

---
+++ Test/Qel.Ef.Test.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace Qel.Ef.Test.Console;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+                         .ConfigureServices((hostingContext, services) =>
+                         {
+                             //services.Add
+                         })
+                         .Build();
+    }
+}
